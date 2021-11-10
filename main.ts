b36d95c467e70b401a8a74948224d8e9b7f023b1
张振凯 <45989662+zhenkai996icu@users.noreply.github.com>
2021-11-10 19:55:19 +0800

    a little test

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic{
+    /**
+     * Address LEDs linearly row first
+     */
+    //% blockId = screenMagicplotat
+    //% block = "plot at index"
+    //% index.min = 0 index.max = 25
     export function plotAt(index: number): void{
         index |= 0
         const x = Math.floor(index / 5);
