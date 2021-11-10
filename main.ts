7f8948aa3a63e8613140421c038187a132a10182
张振凯 <45989662+zhenkai996icu@users.noreply.github.com>
2021-11-10 19:52:35 +0800

    更新 main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-abasic.forever(function () {
-	
-})
+namespace screenMagic{
+    export function plotAt(index: number): void{
+        index |= 0
+        const x = Math.floor(index / 5);
+        const y = Math.floor(index % 5);
+        led.plot(x, y)
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+for(let i = 0; i < 25; ++i){
+    screenMagic.plotAt(i)
+    basic.pause(100)
+}
